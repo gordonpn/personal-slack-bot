cf04055a0f404f8d4172087eafc06e6af92ffcd0
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-05-02 12:13:12 -0400

    fix(ci): remove a command repeated twice

--- .docker/scripts/deploy_dev.sh
+++ .docker/scripts/deploy_dev.sh
@@ -2,4 +2,4 @@
 docker container stop slack-bot_mongo-db
 docker container stop reddit-scraper
 docker container stop slack-bot
-docker-compose -f /drone/src/docker-compose.yml up -f /drone/src/docker-compose.dev.yml up --detach --build
+docker-compose -f /drone/src/docker-compose.yml -f /drone/src/docker-compose.dev.yml up --detach --build
--- .docker/scripts/deploy_prod.sh
+++ .docker/scripts/deploy_prod.sh
@@ -2,4 +2,4 @@
 docker container stop slack-bot_mongo-db-dev
 docker container stop reddit-scraper-dev
 docker container stop slack-bot-dev
-docker-compose -f /drone/src/docker-compose.yml up -f /drone/src/docker-compose.prod.yml up --detach --build
+docker-compose -f /drone/src/docker-compose.yml -f /drone/src/docker-compose.prod.yml up --detach --build
