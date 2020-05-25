52c02d183bf8dbc3ab09da94883d1452ffeff738
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-05-25 14:07:30 -0400

    ci: remove deploy dev from pipeline

--- .docker/scripts/deploy_prod.sh
+++ .docker/scripts/deploy_prod.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
-docker container stop slack-bot_mongo-db-dev
-docker container stop reddit-scraper-dev
-docker container stop slack-bot-dev
+docker container stop slack-bot_mongo-db-dev || true
+docker container stop reddit-scraper-dev || true
+docker container stop slack-bot-dev || true
 docker-compose -f /drone/src/docker-compose.yml -f /drone/src/docker-compose.prod.yml up --detach --build
